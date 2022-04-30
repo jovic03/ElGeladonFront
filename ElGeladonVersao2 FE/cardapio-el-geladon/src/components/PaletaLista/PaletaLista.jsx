e2ce58aabe57d5366d7a346ac5fe2e7cf155b287
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-29 23:08:09 -0300

    Update de PalestaLista e PaletaListaItem para receber dados do Back

--- ElGeladonVersao2 FE/cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
+++ ElGeladonVersao2 FE/cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
@@ -1,14 +1,21 @@
 import './PaletaLista.css';
-import {paletas} from 'mocks/paletas'
-import { useState } from 'react';
+import { PaletaService } from 'services/PaletaService';
+import { useState, useEffect } from 'react';//useEffect: renderiza algo mas faz algo depois, useState vai alterando seu estado
 import PaletaListaItem from 'components/PaletaListaItem/PaletaListaItem';
 
 function PaletaLista() {
 
+  const [paletas, setPaletas] = useState([]);//vai vir da Api
+
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
+  const getLista = async ()=>{//substituto dos mock
+    const response = await PaletaService.getLista();
+    setPaletas(response);
+  }
 
-  const adicionarItem = (paletaIndex) => {
+
+  const OnAdd = (paletaIndex) => {
 
     /*explain linha abaixo: indice da paleta:convertendo pra numero o indice NO vetor da peleta e 
     quantas vezes foi clicada---> ou seja quantas vezes a 
@@ -18,20 +25,27 @@ function PaletaLista() {
     setPaletaSelecionada({...paletaSelecionada, ...paleta});//pega o que tem na variavel e manter e APENAS adiciona
     //console.log(paletaSelecionada);
   }
-  const removerItem = (paletaIndex) => {
+
+  const OnRemove = (paletaIndex) => {
     const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1 }
     setPaletaSelecionada({...paletaSelecionada, ...paleta})
   };
+  
+  useEffect(() => {getLista();
+    }, []);
   
 
     return (
       <div className="PaletaLista">{/*for (int i = 0;i<20;i++) ---tem que ser chave pois aqui é js---paleta pega uma paleta no paletas.js e o index serve pra mapear o indice e serve pro js */}
         {
           paletas.map((paleta,index)=>(
-            <PaletaListaItem key={`PaletaListaItem-${index}`}
-            paleta={paleta}
-            quantidadeSelecionada={paletaSelecionada[index]}
-            index={index} />
+            <PaletaListaItem 
+              key={`PaletaListaItem-${index}`}
+              paleta={paleta}
+              quantidadeSelecionada={paletaSelecionada[index]}
+              index={index}
+              OnAdd={(index)=>{OnAdd(index)}}//tem que passar pra nao ficar em loop infinito de callback
+              OnRemove={(index)=>{OnRemove(index)}} />
           ))
         }
       </div>
--- ElGeladonVersao2 FE/cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
+++ ElGeladonVersao2 FE/cardapio-el-geladon/src/components/PaletaListaItem/PaletaListaItem.jsx
@@ -1,40 +1,28 @@
 import './PaletaListaItem.css';
 
-function PaletaListaItem(){
-  //mocks demo:
-  const removerItem = (i) => console.log('remover' + i);
-  const adicionarItem = (i) => console.log('adicionar' + i);
-  const paletaSelecionada = [0];
-  const index = 0;
-  const paleta = {
-  titulo: "Açaí com Leite Condensado",
-  descricao:
-  "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
-  foto: require("assets/images/acai-com-leite-condensado.png"),
-  preco: 10.0,
-  sabor: "Açaí",
-  recheio: "Leite Condensado",
-  possuiRecheio: true,
-  };
-  //----------fim mocks demos
-    
-    const badgeCounter = (canRender, index) => Boolean(canRender) && (<span className='PaletaListaItem__badge'>{paletaSelecionada[index]}</span>);
+function PaletaListaItem({paleta, quantidadeSelecionada,index, OnRemove,OnAdd}){
+      
+    const badgeCounter = (canRender, index) => 
+      Boolean(canRender) && (
+        <span className='PaletaListaItem__badge'>{quantidadeSelecionada}</span>);
 
-    const removeButton = (canRender, index) => Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
+    const removeButton = (canRender, index) => 
+      Boolean(canRender) && (
+        <button className="Acoes__remover" onClick={() => OnRemove(index)}>remover</button>);
 
     return(
         <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-        {badgeCounter(paletaSelecionada[index], index)}
+        {badgeCounter(quantidadeSelecionada, index)}
               <div>
                 <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
                 <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
                 <div className="PaletaListaItem__descricao"> {paleta.descricao}  </div>
                 <div className="PaletaListaItem__acoes Acoes">
 
-                  <button className={`Acoes__adicionar ${!paletaSelecionada[index] && 'Acoes__adicionar--preencher'}`}
-                  onClick={() => adicionarItem(index)}>adicionar</button>
+                  <button className={`Acoes__adicionar ${!quantidadeSelecionada && 'Acoes__adicionar--preencher'}`}
+                  onClick={() => OnAdd(index)}>adicionar</button>
 
-                  {removeButton(paletaSelecionada[index], index)}
+                  {removeButton(quantidadeSelecionada, index)}
 
                 </div>
               </div>
