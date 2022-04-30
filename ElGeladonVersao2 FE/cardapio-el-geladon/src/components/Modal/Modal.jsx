691e7ec78b7a86a22f710407a57f466b2c85278f
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-29 23:06:05 -0300

    criacao modal

---
+++ ElGeladonVersao2 FE/cardapio-el-geladon/src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import ".Modal/css";
+import Overlay from "components/Overlay/Overlay";
+
+function Modal({ children, closeModal }) {//funcao que vai exibir conteudo dentro dele + vai utilizar o Overlay + como clica fora de um objeto pra fechar o Modal
+  const handleClick = (e, canClose) => {//'e' é o nome do evento, que vai impedir a propagacao para os Listeners da pagina
+    e.stopPropagation();
+    if (canClose) closeModal();
+  };
+
+  return (//o retorno do conteudo acima, no caso fecha o modal se o teste der ok
+    <Overlay overlayClick={closeModal}>
+      <div className="Modal" onClick={handleClick}>
+        <span className="Modal__close" onClick={(e) => handleClick(e, true)}>
+          +
+        </span>
+        <div className="Modal__body">{children}</div>
+      </div>
+    </Overlay>
+  );
+}
+
+export default Modal;
