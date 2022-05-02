824735dfa630a9773542dc6409d3b9ca5c8c6839
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-02 16:59:50 -0300

    update modal

--- ElGeladonVersao2 FE/cardapio-el-geladon/src/components/Modal/Modal.jsx
+++ ElGeladonVersao2 FE/cardapio-el-geladon/src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
-import ".Modal/css";
+import './Modal.css'
 import Overlay from "components/Overlay/Overlay";
 
 function Modal({ children, closeModal }) {//funcao que vai exibir conteudo dentro dele + vai utilizar o Overlay + como clica fora de um objeto pra fechar o Modal
   const handleClick = (e, canClose) => {//'e' é o nome do evento, que vai impedir a propagacao para os Listeners da pagina
-    e.stopPropagation();
+    e.stopPropagation();//impede que o evento seja 'propagado' (exibido)
     if (canClose) closeModal();
   };
 
