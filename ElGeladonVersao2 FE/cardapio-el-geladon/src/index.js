fb06ee581403d39806141b1358864414599738d6
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-06 22:40:00 -0300

    Update index.js

--- ElGeladonVersao2 FE/cardapio-el-geladon/src/index.js
+++ ElGeladonVersao2 FE/cardapio-el-geladon/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./assets/style/index.css";
-import Home from './views/Home';
+import Home from './views/Home';//como quem renderiza a home é o app ele nem precisa estar, e por isso nao é usado
 import App from 'App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
